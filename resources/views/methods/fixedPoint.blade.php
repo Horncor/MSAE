372bc7aacd658286c36ded6893a93a37ec4d9ebf
Jose Vega <horncor2799@gmail.com>
2023-08-05 17:11:16 -0600

    update fix point visual

--- resources/views/methods/fixedPoint.blade.php
+++ resources/views/methods/fixedPoint.blade.php
@@ -9,15 +9,7 @@
     <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
     @include('layouts.style')
     <style>
-        /* Estilo para el contenedor del gráfico */
-        #chartContainer {
-            width: 100%;
-            /* Ancho del 100% del contenedor padre */
-            max-width: 800px;
-            /* Ancho máximo de 800px */
-            margin: 0 auto;
-            /* Centrar el contenedor en la pantalla */
-        }
+
     </style>
 </head>
 
@@ -44,60 +36,62 @@
         <div class="body-wrapper">
             @include('layouts.header')
             <div class="container-fluid">
-                
-                    <div class="d-flex justify-content-end pb-2">
-                        <h4 class="fw-bolder">Metodo de Punto Fijo</h4>
-                    </div>
-                    <div class="d-flex justify-content-between pb-2">
-                        <p class="fs-5">Rellena los campos necesarios</p>
-                        <p class="text-danger fs-5">Todos los campos son requeridos</p>
-                    </div>
-                    <div class="container pb-4">
-                        <div class="row px-2 pb-1">
-                            <div class="col-md has-validation">
-                                <label for="originalFunction" class="form-label">Función original</label>
-                                <input type="text" class="form-control" id="originalFunction"
-                                    aria-describedby="valor1Help">
-                                <div id="valor1Help" class="form-text">Ingrese una función original.</div>
-                            </div>
-                            <div class="col-md has-validation">
-                                <label for="func" class="form-label">Función iterativa</label>
-                                <input type="text" class="form-control" id="func" aria-describedby="valor2Help">
-                                <div id="valor2Help" class="form-text">Ingrese una función iterativa.</div>
-                            </div>
-                            <div class="col-md">
-                                <label for="valueXI" class="form-label">Valor Inicial</label>
-                                <input type="number" class="form-control" id="valueXI" aria-describedby="valor3Help">
-                                <div id="valor3Help" class="form-text">Unicamente se permiten numeros.</div>
-                            </div>
-                            <div class="col-md">
-                                <label for="tolerancia" class="form-label">Tolerancia</label>
-                                <input type="number" class="form-control" id="tolerancia"
-                                    aria-describedby="valor4Help">
-                                <div id="valor4Help" class="form-text">Unicamente se permiten numeros.</div>
-                            </div>
-                            <div class="col-md">
-                                <label class="form-label" for="decimals">Decimales:
-                                    <output class="decimals-output" for="decimals">9</output>
-                                </label>
-                                <input type="range" class="form-range" name="decimals" id="decimals" min="1"
-                                    max="13" step="1" value="9" aria-describedby="decimals" />
-                                <div id="decimals" class="form-text">Selecciona la cantidad de decimales.</div>
-                            </div>
+
+                <div class="d-flex justify-content-end pb-2">
+                    <h4 class="fw-bolder">Metodo de Punto Fijo</h4>
+                </div>
+                <div class="d-flex justify-content-between pb-2">
+                    <p class="fs-5">Rellena los campos necesarios</p>
+                    <p class="text-danger fs-5">Todos los campos son requeridos</p>
+                </div>
+                <div class="container pb-4">
+                    <div class="row px-2 pb-1">
+                        <div class="col-md has-validation">
+                            <label for="originalFunction" class="form-label">Función original</label>
+                            <input type="text" class="form-control" id="originalFunction"
+                                aria-describedby="valor1Help">
+                            <div id="valor1Help" class="form-text">Ingrese una función original.</div>
+                        </div>
+                        <div class="col-md has-validation">
+                            <label for="func" class="form-label">Función iterativa</label>
+                            <input type="text" class="form-control" id="func" aria-describedby="valor2Help">
+                            <div id="valor2Help" class="form-text">Ingrese una función iterativa.</div>
+                        </div>
+                        <div class="col-md">
+                            <label for="valueXI" class="form-label">Valor Inicial</label>
+                            <input type="number" class="form-control" id="valueXI" aria-describedby="valor3Help">
+                            <div id="valor3Help" class="form-text">Unicamente se permiten numeros.</div>
+                        </div>
+                        <div class="col-md">
+                            <label for="tolerancia" class="form-label">Tolerancia</label>
+                            <input type="number" class="form-control" id="tolerancia" aria-describedby="valor4Help">
+                            <div id="valor4Help" class="form-text">Unicamente se permiten numeros.</div>
+                        </div>
+                        <div class="col-md">
+                            <label class="form-label" for="decimals">Decimales:
+                                <output class="decimals-output" for="decimals">9</output>
+                            </label>
+                            <input type="range" class="form-range" name="decimals" id="decimals" min="1"
+                                max="13" step="1" value="9" aria-describedby="decimals" />
+                            <div id="decimals" class="form-text">Selecciona la cantidad de decimales.</div>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-between pb-2">
-                        <p class="fs-5">Resultados de la operacion</p>
-                        <button type="button" onclick="validateFunc()" class="btn btn-primary">Realizar
-                            Calculo</button>
-                    </div>
                 </div>
-                <br>
-                <div id="table-result-operation">
+                <div class="d-flex justify-content-between pb-2">
+                    <p class="fs-5">Resultados de la operacion</p>
+                    <button type="button" onclick="validateFunc()" class="btn btn-primary">Realizar
+                        Calculo</button>
+                </div>
+            </div>
+            <br>
+            <div id="table-result-operation">
 
+            </div>
+            <div class="row">
+                <div class="col-md">
+                    <div id="graphDiv"  style="border: 1px solid aqua;"></div>
                 </div>
-                <div id="graphDiv"></div>
-            
+            </div>
         </div>
         @include('layouts.script')
         <script src="{{ asset('assets/js/methodfixedPoint.js') }}"></script>
